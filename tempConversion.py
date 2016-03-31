026666c017ac05e4b57d34846a90eb76a7d6a132
Phill Conrad <pconrad@cs.ucsb.edu>
2016-03-31 09:53:11 -0700

    Create tempConversion.py

---
+++ tempConversion.py
@@ -0,0 +1,13 @@
+# tempConversion.py   A module for converting between fahrenheit and celsius
+
+# The following starting point code passes some test cases, 
+# but it fails others.  Your job in this lab exercise is to modify the code
+# so that is passes all of the test cases
+
+def fToC(ftemp):
+  ''' convert fahrenheit to celsius '''
+  return ftemp - 32.0   # TODO: Fix this line of code
+  
+def cToF(ctemp):
+  ''' convert celsius to fahrenheit '''
+  return ctemp + 32.0   # TODO: Fix this line of code
